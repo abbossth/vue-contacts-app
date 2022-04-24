4da212d2bdc9083d30508db9fd2f18d2ea56ce8d
abbossth <bosskodevelopment@gmail.com>
2022-04-24 23:52:57 +0500

    Bootstrap 4 binding and basic Header

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand text-uppercase" href="#">Contacts</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contacts List <span class="sr-only">(current)</span></a>
+        </li>
+      </ul>
+      <a href="#" class="btn btn-primary">New Contact</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'HeaderNavbar'
+}
+</script>
+
+<style>
+
+</style>
