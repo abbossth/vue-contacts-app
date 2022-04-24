9a924181eefc15e3f6e262b6140302a607884416
abbossth <bosskodevelopment@gmail.com>
2022-04-25 00:11:27 +0500

    Vue-router is added

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand text-uppercase" href="#">Contacts</a>
+    <a class="navbar-brand text-uppercase font-weight-bold" href="#">Контакты</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -8,10 +8,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-          <a class="nav-link" href="#">Contacts List</a>
+          <a class="nav-link" href="#">Все Контакты</a>
         </li>
       </ul>
-      <a href="#" class="btn btn-outline-success">New Contact</a>
+      <a href="#" class="btn btn-outline-success">Новый Контакт</a>
     </div>
   </nav>
 </template>
