3efc99026485f3e61dee871836d4149a4d5c3501
abbossth <bosskodevelopment@gmail.com>
2022-04-27 03:14:29 +0500

    Reading Email and Phone data

--- src/components/SingleContact.vue
+++ src/components/SingleContact.vue
@@ -1,6 +1,18 @@
 <template>
   <p> <b>Name: </b> {{ contact.name}}</p>
-  <p><b>ID: </b> {{ contact.id }}</p>
+  <p><b>Id: </b> {{ contact.id }}</p>
+  <div>
+    <b>Phone: </b>
+    <div v-for="phone in contact.phone" :key="phone">
+      {{ phone }}
+    </div>
+  </div>
+  <div>
+    <b>Email: </b>
+    <div v-for="email in contact.email" :key="email">
+      {{ email }}
+    </div>
+  </div>
   <router-link :to="'/updateContact/' + contact.id">
     <span class="fas fa-pen"></span>
   </router-link>
