3d400db466712bcee5f64724c5080b432470c6cc
abbossth <bosskodevelopment@gmail.com>
2022-04-26 23:22:55 +0500

    Search tool is implemented in the app

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -1,6 +1,14 @@
 <template>
-  <div id="accordion">
-    <div class="card" v-for="contact in allContacts" :key="contact.id">
+  <div class="input-group mb-3">
+    <div class="input-group-prepend">
+      <span class="input-group-text bg-white">
+        <i class="fas fa-search"></i>
+      </span>
+    </div>
+    <input type="text" class="form-control" v-model="searched" placeholder="Поиск по именам">
+  </div> 
+  <div id="accordion" v-if="isAvailable(allContacts)">
+    <div class="card" v-for="contact in filteredContacts" :key="contact.id">
       <div class="card-header d-flex justify-content-between" id="headingOne" data-toggle="collapse" :data-target="'#'+contact.id" aria-expanded="true" aria-controls="collapseOne">
         <p class="mb-0 p-1">
           <span>
@@ -15,6 +23,13 @@
       </div>
     </div>
   </div>
+  <div class="alert alert-warning " v-else>
+    Нет доступных контактов...
+    <router-link to="/newContact" class="text-success">
+      <span class="mx-1">Нажмите, чтобы создать новый</span>
+      <i class="fas fa-plus"></i>
+    </router-link>
+  </div>
 </template>
 
 <script>
@@ -22,18 +37,32 @@ import { mapGetters, mapActions } from 'vuex'
 import SingleContact from './SingleContact'
 export default {
   name: "ContactsList",
+  data() {
+    return {
+      searched: ''
+    }
+  },
   methods: {
     ...mapActions(['fetchContacts', 'deleteContact','fetchSingleContact']),
+    isAvailable(list) {
+      return list.length === 0 ? false : true 
+    }
   },
-  computed: mapGetters(['allContacts']),
+  computed: {
+    ...mapGetters(['allContacts']),
+    filteredContacts: function() {
+      return this.allContacts.filter((contact) => {
+        return contact.name.toLowerCase().match(this.searched)
+      })
+    }
+  },
   created() {
     this.fetchContacts(),
     this.fetchSingleContact()
   },
   components: {
     SingleContact
-  },
-  emits: ['singleContactId']
+  }
 }
 </script>
 
--- src/views/AllContacts.vue
+++ src/views/AllContacts.vue
@@ -1,17 +1,14 @@
 <template>
   <h3 class="text-center mt-3 pb-2 border-bottom">Все Контакты</h3>
-  <Search />
   <Contacts />
 </template>
 
 <script>
 import Contacts from '../components/Contacts'
-import Search from '../components/Search'
 export default {
   name: 'AllContacts',
   components: {
-    Contacts,
-    Search
+    Contacts
   }
 }
 </script>
